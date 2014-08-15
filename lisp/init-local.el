53604672a758fa687f91c759182ea9ef484c4cb2
liuenze <liu.enze@me.com>
2014-08-15 13:50:11 +0800

    update : add projectile to search file;add evil to use vim key bindings

---
+++ lisp/init-local.el
@@ -0,0 +1,40 @@
+(require 'evil-leader)
+
+(global-evil-leader-mode)
+
+(evil-leader/set-key
+  "e" 'find-file
+  "b" 'switch-to-buffer
+  "k" 'kill-buffer
+  "w" 'save-buffer
+  "q" 'save-buffers-kill-terminal)
+
+(evil-leader/set-leader ",")
+
+(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
+
+(evil-define-command cofi/maybe-exit ()
+                     :repeat change
+                     (interactive)
+                     (let ((modified (buffer-modified-p)))
+                       (insert "j")
+                       (let ((evt (read-event (format "Insert %c to exit insert state" ?j)
+                                              nil 0.5)))
+                         (cond
+                           ((null evt) (message ""))
+                           ((and (integerp evt) (char-equal evt ?j))
+                            (delete-char -1)
+                            (set-buffer-modified-p modified)
+                            (push 'escape unread-command-events))
+                           (t (setq unread-command-events (append unread-command-events
+                                                                  (list evt))))))))
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (evil-mode)))
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (projectile-global-mode)))
+
+(provide 'init-local)
