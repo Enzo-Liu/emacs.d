0c60543ef1fd5abd98ab6cc9323dd1d8b3db53b5
liuenze <liu.enze@me.com>
2015-08-09 15:01:10 +0800

    update

--- local/enzo-eclim.el
+++ local/enzo-eclim.el
@@ -1,18 +1,18 @@
-(require-package 'emacs-eclim)
-(require 'eclim)
-(require 'eclimd)
-(global-eclim-mode)
+;; (require-package 'emacs-eclim)
+;; (require 'eclim)
+;; (require 'eclimd)
+;; (global-eclim-mode)
 
-(custom-set-variables
- '(eclim-eclipse-dirs '("~/eclim/eclipse"))
- '(eclim-executable "~/eclim/eclipse/eclim")
- '(eclimd-executable "~/eclim/eclipse/eclimd"))
+;; (custom-set-variables
+;;  '(eclim-eclipse-dirs '("~/eclim/eclipse"))
+;;  '(eclim-executable "~/eclim/eclipse/eclim")
+;;  '(eclimd-executable "~/eclim/eclipse/eclimd"))
 
-(require-package 'ensime)
-(require-package 'sbt-mode)
-(require-package 'scala-mode2)
+;; (require-package 'ensime)
+;; (require-package 'sbt-mode)
+;; (require-package 'scala-mode2)
 
-(require 'ac-emacs-eclim-source)
-(ac-emacs-eclim-config)
+;; (require 'ac-emacs-eclim-source)
+;; (ac-emacs-eclim-config)
 
 (provide 'enzo-eclim)
