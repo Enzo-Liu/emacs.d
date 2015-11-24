4b7c2cee58b46f636c16abf6fd7f49f56ccfa085
Enzo Liu <liuenze6516@gmail.com>
2015-11-24 18:18:06 +0800

    update emacs config about backupfile
           and local safe values

--- lisp/init-preload-local.el
+++ lisp/init-preload-local.el
@@ -1,5 +1,5 @@
 (setf user-login-name "enzo-liu")
-(setq socks-noproxy '("127.0.0.1"))
+(setq socks-noproxy '("127.0.0.1" "121.42.53.178"))
 (setq socks-server '("Default server" "127.0.0.1" 1080 5))
 (setq url-gateway-method 'socks)
 
